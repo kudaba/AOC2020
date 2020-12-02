bd63fef733e5d60efbf8de8e543f20ff72abf171
Chris Savoie <chris@chrissavoie.com>
2020-12-01 21:41:45 -0800

    Day2 - Fix CWD to be source folder to help file loading

---
+++ AOC/AOC_Day2.cpp
@@ -0,0 +1,63 @@
+#include "AOC_Precompiled.h"
+#include "AOC_Day2.h"
+
+static uint locGetValidPW(char const* data)
+{
+	uint valid = 0;
+
+	GC_StrSlice line;
+	while (GC_StrLine(data, line))
+	{
+		int min, max;
+		char c;
+		char pw[32];
+		_snscanf(line.Buffer(), line.Count(), "%d-%d %c: %31s", &min, &max, &c, pw);
+
+		int count = 0;
+		for (char ch : pw)
+		{
+			if (!ch) break;
+			if (ch == c)
+				++count;
+		}
+
+		if (GC_InRange(count, min, max))
+			++valid;
+	}
+	return valid;
+}
+
+DEFINE_TEST(Day2Part1)
+{
+	TEST_EQ(locGetValidPW(testData), 2);
+	GC_String text;
+	GC_File::ReadAllText("AOC_Day2Part1.txt", text);
+	TEST_EQ(locGetValidPW(text), 396);
+}
+
+static uint locGetValidPW2(char const* data)
+{
+	uint valid = 0;
+
+	GC_StrSlice line;
+	while (GC_StrLine(data, line))
+	{
+		int min, max;
+		char c;
+		char pw[32];
+		_snscanf(line.Buffer(), line.Count(), "%d-%d %c: %31s", &min, &max, &c, pw);
+
+		if ((pw[min - 1] == c) != (pw[max - 1] == c))
+			++valid;
+	}
+	return valid;
+}
+
+DEFINE_TEST(Day2Part2)
+{
+	TEST_EQ(locGetValidPW2(testData), 1);
+	GC_String text;
+	Printf("%s\n", GC_Path::GetAbsolutePath("Day2Part1.txt").ToString());
+	GC_File::ReadAllText("AOC_Day2Part1.txt", text);
+	TEST_EQ(locGetValidPW2(text), 428);
+}
--- AOC/AOC_Precompiled.cpp
+++ AOC/AOC_Precompiled.cpp
@@ -1,6 +1,15 @@
 #include "AOC_Precompiled.h"
+#include "GC_Initialize.h"
 
 int main(int argc, char* argv[])
 {
+	// Initialize GC once to commit CWD
+	GC_InitializeSettings settings("UnitTests");
+	settings.LogFile = nullptr; // disable logging since it's costly and gets overwritten on each test
+	settings.TaskPoolsEnabled = false; // unit tests don't get the thread pool, they need to control their threads manually
+	settings.BranchToCurrentDirectory = "AOC";
+	GC_Initialize(settings);
+	GC_Shutdown();
+
 	return GC_TestFixture::RunAll(argc, argv);
 }
