433ea774ab299c86afa79c383e529a59ffdf936f
Chris Savoie <chris@chrissavoie.com>
2020-12-05 21:15:38 -0800

    Day 6

--- AOC/AOC_Day6.cpp
+++ AOC/AOC_Day6.cpp
@@ -1,12 +1,62 @@
 #include "AOC_Precompiled.h"
 #include "AOC_Day6.h"
+#include "GC_Integer.h"
+
+static uint countUnique(char const* aFile)
+{
+	uint sum = 0;
+	GC_String text;
+	GC_File::ReadAllText(aFile, text);
+	for (GC_StrSlice blocks; GC_Strtok(text, "\n\n", blocks);)
+	{
+		uint v = 0;
+		for (GC_StrSlice line; GC_StrLine(blocks, line);)
+		{
+			for (char c : line)
+			{
+				v |= 1 << (c - 'a');
+			}
+		}
+
+		sum += GC_GetNumBitsSet(v);
+	}
+	return sum;
+}
 
 DEFINE_TEST_G(Part1, Day6)
 {
+	TEST_EQ(countUnique("AOC_Day6Test.txt"), 11);
+	TEST_EQ(countUnique("AOC_Day6Part1.txt"), 6885);
+}
 
+
+static uint countAll(char const* aFile)
+{
+	uint sum = 0;
+	GC_String text;
+	GC_File::ReadAllText(aFile, text);
+	for (GC_StrSlice blocks; GC_Strtok(text, "\n\n", blocks);)
+	{
+		uint v[26] = { 0 };
+		uint count = 0;
+		for (GC_StrSlice line; GC_StrLine(blocks, line);)
+		{
+			for (char c : line)
+			{
+				v[(c - 'a')]++;
+			}
+			++count;
+		}
+
+		for (uint vv : v)
+			if (vv == count)
+				++sum;
+	}
+	return sum;
 }
 
 DEFINE_TEST_G(Part2, Day6)
 {
-
+	TEST_EQ(countAll("AOC_Day6Test.txt"), 6);
+	TEST_EQ(countAll("AOC_Day6Part1.txt"), 3550);
 }
