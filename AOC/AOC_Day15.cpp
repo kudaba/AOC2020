f2856d7ac3971efdce8aa2d5008726faa5d8182f
Chris Savoie <chris@chrissavoie.com>
2020-12-14 21:38:51 -0800

    Day 15

--- AOC/AOC_Day15.cpp
+++ AOC/AOC_Day15.cpp
@@ -1,39 +1,40 @@
 #include "AOC_Precompiled.h"
 #include "AOC_Day15.h"
 
-static uint locDay15Part1(char const* aFile)
+static uint locDay15(char const* aFile, uint count)
 {
-	uint result = 0;
-
-	for (auto line : GC_File::ReadAllLines(aFile))
-	{
-
-	}
-
 	GC_String text;
 	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
 
+	GC_DynamicArray<uint> spokenMap;
+	spokenMap.Resize(count);
+
+	uint i = 1;
+	for (GC_StrSlice chunk; GC_Strtok(text, ",", chunk); i++)
+	{
+		spokenMap[GC_Atoi(chunk)] = i;
 	}
 
-	return result;
+	uint last = 0;
+
+	for (; i < count; ++i)
+	{
+		uint index = spokenMap[last];
+		spokenMap[last] = i;
+		last = (i - index) * !!index;
+	}
+
+	return last;
 }
 
 DEFINE_TEST_G(Part1, Day15)
 {
-	TEST_EQ(locDay15Part1("AOC_Day15Test.txt"), 0);
-	TEST_EQ(locDay15Part1("AOC_Day15Part1.txt"), 0);
-}
-
-static uint locDay15Part2(char const* aFile)
-{
-	GC_UNUSED(aFile);
-	return 0;
+	TEST_EQ(locDay15("AOC_Day15Test.txt", 2020), 436);
+	TEST_EQ(locDay15("AOC_Day15Part1.txt", 2020), 257);
 }
 
 DEFINE_TEST_G(Part2, Day15)
 {
-	TEST_EQ(locDay15Part2("AOC_Day15Test.txt"), 0);
-	TEST_EQ(locDay15Part2("AOC_Day15Part1.txt"), 0);
+	TEST_EQ(locDay15("AOC_Day15Test.txt", 30000000), 175594);
+	TEST_EQ(locDay15("AOC_Day15Part1.txt", 30000000), 8546398);
 }
