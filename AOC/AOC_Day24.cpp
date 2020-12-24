f5677f870d3a5445191301b59d2f11f04cdb4309
Chris Savoie <chris@chrissavoie.com>
2020-12-23 21:30:19 -0800

    Day 24

--- AOC/AOC_Day24.cpp
+++ AOC/AOC_Day24.cpp
@@ -1,39 +1,97 @@
 #include "AOC_Precompiled.h"
 #include "AOC_Day24.h"
 
-static uint locDay24Part1(char const* aFile)
+static uint locDay24(char const* aFile, uint numDays)
 {
-	uint result = 0;
+	GC_HashSet<GC_Vector2i> blackTiles;
 
 	for (auto line : GC_File::ReadAllLines(aFile))
 	{
+		GC_Vector2i point(0);
+		for (char const* b = line.begin(); b != line.end(); ++b)
+		{
+			switch (*b)
+			{
+			case 'e':
+				point.x += 1;
+				break;
+			case 'w':
+				point.x -= 1;
+				break;
+			case 's':
+				if (b[1] == 'e')
+					point += GC_Vector2i(1, -1);
+				else
+					point += GC_Vector2i(0, -1);
+				++b;
+				break;
+			case 'n':
+				if (b[1] == 'e')
+					point += GC_Vector2i(0, 1);
+				else
+					point += GC_Vector2i(-1, 1);
+				++b;
+				break;
+			}
+		}
 
+		if (blackTiles.Contains(point))
+			blackTiles.Remove(point);
+		else
+			blackTiles.Add(point);
 	}
 
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
+	GC_Vector2i directions[] =
 	{
+		{1,0},
+		{-1,0},
+		{0,1},
+		{-1,1},
+		{0,-1},
+		{1,-1},
+	};
 
+	GC_HashMap<GC_Vector2i, uint> counts;
+	for_range(numDays)
+	{
+		counts.Clear();
+
+		for (GC_Vector2i t : blackTiles)
+		{
+			for (auto v : directions)
+			{
+				auto count = counts.Add(t + v);
+				if (count.myFirst)
+					*count.mySecond = 1;
+				else
+					++(*count.mySecond);
+			}
+
+			auto count = counts.Add(t);
+			if (count.myFirst)
+				*count.mySecond = 0;
+		}
+
+		for (auto v : counts)
+		{
+			if (v.myValue == 2)
+				blackTiles.Add(v.myKey);
+			else if (v.myValue != 1)
+				blackTiles.Remove(v.myKey);
+		}
 	}
 
-	return result;
+	return blackTiles.Count();
 }
 
 DEFINE_TEST_G(Part1, Day24)
 {
-	TEST_EQ(locDay24Part1("AOC_Day24Test.txt"), 0);
-	TEST_EQ(locDay24Part1("AOC_Day24Part1.txt"), 0);
-}
-
-static uint locDay24Part2(char const* aFile)
-{
-	GC_UNUSED(aFile);
-	return 0;
+	TEST_EQ(locDay24("AOC_Day24Test.txt", 0), 10);
+	TEST_EQ(locDay24("AOC_Day24Part1.txt", 0), 263);
 }
 
 DEFINE_TEST_G(Part2, Day24)
 {
-	TEST_EQ(locDay24Part2("AOC_Day24Test.txt"), 0);
-	TEST_EQ(locDay24Part2("AOC_Day24Part1.txt"), 0);
+	TEST_EQ(locDay24("AOC_Day24Test.txt", 100), 2208);
+	TEST_EQ(locDay24("AOC_Day24Part1.txt", 100), 3649);
 }
