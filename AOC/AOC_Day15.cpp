7f4d56889befd8c523e4547109d2c8b7fbbcb050
Chris Savoie <chris@chrissavoie.com>
2021-12-16 23:15:36 -0800

    Optimized Day 15 to use proper dijkstra's. Went from 8-12s in reelase visiting 630M tiles to 40ms visiting 500k tiles.

--- AOC/AOC_Day15.cpp
+++ AOC/AOC_Day15.cpp
@@ -2,37 +2,58 @@
 
 struct Tile
 {
-	int Cost;
-	uint MaxCost;
+	char Cost : 6;
+	char Visited : 2;
 };
 
-static void locGetLowestCost(GC_DynamicArray2D<Tile>& data, GC_Vector2u pos, GC_Vector2u lastPos, uint cost, uint& maxcost, uint& iterations)
+static uint locGetLowestCost(GC_DynamicArray2D<Tile>& data)
 {
-	++iterations;
-	Tile& t = data(pos);
-	if (t.MaxCost <= cost) return;
-	t.MaxCost = cost;
+	auto const end = data.Size() - GC_Vector2u(1);
+	GC_HashMap<uint, GC_HybridArray<GC_Vector2u, 32>> queue;
+	queue.Reserve(data.Size().Area());
+	queue.Add(0, GC_HybridArray<GC_Vector2u, 32>(1, { 0,0 }));
 
-	auto distance = data.Size() - pos - GC_Vector2u(1);
-	if (maxcost > cost + (distance.x + distance.y) * 2)
+	uint iterations = 0;
+	uint cost = 0;
+
+	while (queue.Count())
 	{
-		if (pos == data.Size() - GC_Vector2u(1))
+		GC_HybridArray<GC_Vector2u, 32>* costQueue = queue.Find(cost);
+		while (!costQueue)
 		{
-			if (cost < maxcost)
-			{
-				maxcost = cost;
-			}
+			++cost;
+			costQueue = queue.Find(cost);
 		}
-		else
+		++iterations;
+
+		auto const candidate = costQueue->Last();
+		costQueue->PopBack();
+		if (costQueue->IsEmpty())
+			queue.Remove(cost);
+
+		auto& tile = data(candidate);
+
+		if (tile.Visited) continue;
+		tile.Visited = true;
+
+		if (end == candidate)
 		{
-			for (auto dir : GC_Cardinal::Range())
+			TestFixture::GetCurrentTest()->Printf("iters: %d\n", iterations);
+			return cost;
+		}
+
+		for (auto dir : GC_Cardinal::Range())
+		{
+			GC_Vector2u next = GC_Vector2u(candidate) + GC_CardinalDirection(dir);
+			if (next < data.Size())
 			{
-				GC_Vector2u next = pos + GC_CardinalDirection(dir);
-				if (next != lastPos && next < data.Size())
-					locGetLowestCost(data, next, pos, cost + data(next).Cost, maxcost, iterations);
+				auto const& ntile = data(next);
+				if (!ntile.Visited)
+					queue.GetOrAdd(cost + ntile.Cost).Add(next);
 			}
 		}
 	}
+	return 0;
 }
 
 static void locExpandBoard(GC_DynamicArray2D<Tile>& data, uint extraSize)
@@ -68,29 +89,9 @@ static void locExpandBoard(GC_DynamicArray2D<Tile>& data, uint extraSize)
 
 static auto locPart1(char const* aFile, uint extraSize = 0)
 {
-	auto data = GC_File::Parse2d<Tile>(aFile, [](char c) { return Tile{ c - '0', UINT_MAX }; });
+	auto data = GC_File::Parse2d<Tile>(aFile, [](char c) { return Tile{ c - '0', false }; });
 	locExpandBoard(data, extraSize);
-
-	// prime data with a max path equivalent.
-	// including the cost of tile 0,0 allows the algorithm to run through paths at least once without failing
-	uint ycost = 0;
-	for_range_v(y, data.Height())
-	{
-		uint xcost = 0;
-		for_range_v(x, data.Width())
-		{
-			auto& t = data(x, y);
-			xcost += t.Cost;
-			t.MaxCost = GC_Min(t.MaxCost, ycost + xcost);
-		}
-		ycost += data(0, y).Cost;
-	}
-
-	uint maxCost = GC_Algorithm::Sum(data.Row(0), [](auto& c) { return c.Cost; }) + GC_Algorithm::Sum(data.Column(data.Width() - 1), [](auto& c) { return c.Cost; });
-	uint iterations = 0;
-	locGetLowestCost(data, GC_Vector2u(0), GC_Vector2u(0), 0, maxCost, iterations);
-	TestFixture::GetCurrentTest()->Printf("iters: %d\n", iterations);
-	return maxCost;
+	return locGetLowestCost(data);
 }
 
 DEFINE_TEST_G(Part1, Day15)
