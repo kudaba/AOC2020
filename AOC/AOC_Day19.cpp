526af6dc6ffdbcabe0350f1d2ad62f633e356432
Chris Savoie <chris@chrissavoie.com>
2021-12-18 23:43:01 -0800

    Day 19

--- AOC/AOC_Day19.cpp
+++ AOC/AOC_Day19.cpp
@@ -1,53 +1,122 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
+struct Scanner
 {
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
+	GC_DynamicArray<GC_Vector3i> Beacons;
+	GC_Vector3i Position = GC_Vector3i(0);
+	bool Fixed = false;
+};
+
+static bool locCompareScanners(GC_HashSet<GC_Vector3i>& finalPositions, Scanner& targetScanner, GC_HashSet<GC_Vector3i>& adjustedPositions)
+{
+	GC_Vector3i orders[] =
+	{
+		GC_Vector3i(0, 1, 2),
+		GC_Vector3i(1, 2, 0),
+		GC_Vector3i(2, 0, 1),
+		GC_Vector3i(2, 1, 0),
+		GC_Vector3i(1, 0, 2),
+		GC_Vector3i(0, 2, 1),
+	};
+
+	for (auto o : orders)
+		for (int x = -1; x <= 1; x += 2)
+			for (int y = -1; y <= 1; y += 2)
+				for (int z = -1; z <= 1; z += 2)
+				{
+					adjustedPositions.Clear();
+					for (auto const& p : targetScanner.Beacons)
+						adjustedPositions.Add(
+							GC_Vector3i(
+								p[o[0]] * x,
+								p[o[1]] * y,
+								p[o[2]] * z
+							));
+
+					for (auto const& anchor : adjustedPositions)
+					{
+						for (auto const& soff : finalPositions)
+						{
+							auto const offset = soff - anchor;
+
+							uint overlap = 0;
+							for (auto const& p : adjustedPositions)
+								if (finalPositions.Contains(p + offset))
+									if (++overlap == 12)
+									{
+										targetScanner.Position = offset;
+										for (auto np : adjustedPositions)
+											finalPositions.Add(np + offset);
+										return true;
+									}
+						}
+					}
+				}
+	return false;
 }
 
-static auto locPart1(char const* aFile)
+static auto locPart1(char const* aFile, uint64& part2)
 {
-	uint64 result = 0;
+	GC_DynamicArray<Scanner> scanners;
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
-
-	// By line parsing
-	for (auto line : GC_File::ReadAllLines(aFile))
-	{
-	}
-
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
+	GC_String text = GC_File::ReadAllText(aFile);
 	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
 	{
+		auto& scanner = scanners.Add().Beacons;
 
+		GC_StrSlice line;
+		GC_StrLine(chunk, line);
+		while (GC_StrLine(chunk, line))
+		{
+			auto& beacon = scanner.Add();
+			for_index(GC_StrSlice axis; GC_Strtok(line, ",", axis);)
+			{
+				beacon[i] = GC_Atoi(axis);
+			}
+		}
 	}
 
-	return result;
+	scanners[0].Fixed = true;
+
+	GC_HashSet<GC_Vector3i> finalPositions(scanners[0].Beacons);
+	GC_HashSet<GC_Vector3i> adjustedPositions;
+
+	while (1)
+	{
+		bool changed = false;
+
+		for(auto& s : scanners)
+		{
+			if (!s.Fixed)
+			{
+				if (locCompareScanners(finalPositions, s, adjustedPositions))
+				{
+					s.Fixed = changed = true;
+				}
+			}
+		}
+
+		if (!changed)
+			break;
+	}
+
+	part2 = 0;
+	for_range(scanners.Count())
+		for_range_v(j, scanners.Count())
+	{
+		GC_Vector3i diff = scanners[i].Position - scanners[j].Position;
+		uint64 sum = GC_Abs(diff.x) + GC_Abs(diff.y) + GC_Abs(diff.z);
+		part2 = GC_Max(sum, part2);
+	}
+
+	return finalPositions.Count();
 }
 
-DEFINE_TEST_G(Part1, Day19)
+DEFINE_TEST_G(Part1And2, Day19)
 {
-	TEST_EQ(locPart1("AOC_Day19Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day19Part1.txt"), 0);
-}
-
-static auto locPart2(char const*)
-{
-	uint64 result = 0;
-	return result;
-}
-
-DEFINE_TEST_G(Part2, Day19)
-{
-	TEST_EQ(locPart2("AOC_Day19Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day19Part1.txt"), 0);
+	uint64 part2;
+	TEST_EQ(locPart1("AOC_Day19Test.txt", part2), 79);
+	TEST_EQ(part2, 3621);
+	TEST_EQ(locPart1("AOC_Day19Part1.txt", part2), 318);
+	TEST_EQ(part2, 12166);
 }
