15df2f52a2e22e6317a69de306b0b9bc8e9ed4b6
Chris Savoie <chris@chrissavoie.com>
2021-12-19 23:01:26 -0800

    Day 20 - optimized

--- AOC/AOC_Day20.cpp
+++ AOC/AOC_Day20.cpp
@@ -1,13 +1,10 @@
 #include "AOC_Precompiled.h"
 
-static auto locPart1(char const* aFile, int iters)
+static auto locPart1(char const* aFile, int iters, bool floppy = false)
 {
-
 	GC_String text = GC_File::ReadAllText(aFile);
 	auto parts = GC_StrSplit<2>(text, "\n\n");
-
 	auto converter = parts[0];
-
 	auto imageData = GC_StrSplit(parts[1], "\n");
 
 	GC_Vector2u const isize = GC_Vector2u(imageData.Count(), imageData[0].Count());
@@ -16,32 +13,33 @@ static auto locPart1(char const* aFile, int iters)
 	GC_Vector2u const hsize = extra / 2;
 
 	GC_DynamicArray2D<bool> image;
-	image.SetSize(size);
-	GC_MemZero(image.Buffer(), image.SizeInBytes());
+	GC_Algorithm::Resize(image, size, false);
+	GC_DynamicArray2D<bool> temp;
+	GC_Algorithm::Resize(temp, size, false);
 
 	for_range2d(imageData.Count(), imageData[0].Count())
 		image(x + hsize.x, y + hsize.y) = imageData[y][x] == '#';
 
-	GC_DynamicArray2D<bool> output;
-	output.SetSize(size);
-
 	for_range(iters)
 	{
-		for_range2d(size.x, size.y)
+		auto input = image.Area(hsize - GC_Vector2u(i), size - hsize + GC_Vector2u(i));
+		auto output = temp.Area(hsize - GC_Vector2u(i+1), size - hsize + GC_Vector2u(i+1));
+
+		for_range2d(output.Width(), output.Height())
 		{
 			uint value = 0;
 
 			for_range2d_v(xx, yy, 3, 3)
 			{
-				GC_Vector2i const pos(x + xx - 1, y + yy - 1);
-				bool const lit = (pos >= GC_Vector2i(0) && pos < (GC_Vector2i)size) ? image(pos) : image(0,0); // use image 0 as the flashing indicator for the infinite
+				GC_Vector2u const pos(x + xx - 2, y + yy - 2);
+				bool const lit = (pos < input.Size()) ? input(pos) : (floppy && (i&1)); // use image 0 as the flashing indicator for the infinite
 				value = (value << 1) | int(lit);
 			}
 
 			output(x, y) = converter[value] == '#';
 		}
 
-		image.Swap(output);
+		image.Swap(temp);
 	}
 
 	return GC_Algorithm::Sum(image.AsRange(), [](bool b) {return int(b); });
@@ -50,11 +48,11 @@ static auto locPart1(char const* aFile, int iters)
 DEFINE_TEST_G(Part1, Day20)
 {
 	TEST_EQ(locPart1("AOC_Day20Test.txt", 2), 35);
-	TEST_EQ(locPart1("AOC_Day20Part1.txt", 2), 5765);
+	TEST_EQ(locPart1("AOC_Day20Part1.txt", 2, true), 5765);
 }
 
 DEFINE_TEST_G(Part2, Day20)
 {
 	TEST_EQ(locPart1("AOC_Day20Test.txt", 50), 3351);
-	TEST_EQ(locPart1("AOC_Day20Part1.txt", 50), 18509);
+	TEST_EQ(locPart1("AOC_Day20Part1.txt", 50, true), 18509);
 }
