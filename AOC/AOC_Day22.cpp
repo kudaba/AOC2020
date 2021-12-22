d497262c6fd2dc6a45aac0387615f76d740d277c
Chris Savoie <chris@chrissavoie.com>
2021-12-21 23:42:27 -0800

    Day 22

--- AOC/AOC_Day22.cpp
+++ AOC/AOC_Day22.cpp
@@ -1,53 +1,102 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
+static uint64 locVolume(GC_Vector3i const& min, GC_Vector3i const& max)
 {
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
+	auto volume = max - min + GC_Vector3i(1); // inclusive of whole range
+	return (uint64)volume.x * volume.y * volume.z;
 }
 
-static auto locPart1(char const* aFile)
+struct Cube
 {
-	uint64 result = 0;
+	GC_Vector3i min;
+	GC_Vector3i max;
 
-	for (auto item : locParseData(aFile))
+	GC_DynamicArray<Cube> FlipRegion;
+
+	void Flip(GC_Vector3i const& newMin, GC_Vector3i const& newMax)
 	{
-		(void)item;
+		for (uint ci = 0; ci < FlipRegion.Count(); ++ci)
+		{
+			Cube& c = FlipRegion[ci];
+
+			bool overlap = true;
+			for_range(3)
+				if (newMax[i] < c.min[i] || newMin[i] > c.max[i])
+					overlap = false;
+
+			if (!overlap)
+				continue;
+
+			// remove whole thing
+			if (newMax >= c.max && newMin <= c.min)
+			{
+				FlipRegion.RemoveAtFast(ci--);
+			}
+			else
+			{
+				c.Flip(GC_Max(c.min, newMin), GC_Min(c.max, newMax));
+			}
+		}
+
+		FlipRegion.Add({newMin, newMax});
 	}
 
-	// By line parsing
+	uint64 Volume() const
+	{
+		return locVolume(min, max) - GC_Algorithm::Sum(FlipRegion, [](auto const& c) { return c.Volume(); });
+	}
+};
+
+static auto locPart1(char const* aFile, int aRange)
+{
+	Cube cubes{GC_Vector3i(-aRange), GC_Vector3i(aRange)};
+	cubes.FlipRegion.Reserve(500);
+
 	for (auto line : GC_File::ReadAllLines(aFile))
 	{
+		auto parts = GC_StrSplit<2>(line, " ");
+		auto rangeStrings = GC_StrSplit<3>(parts[1], ",");
+
+		GC_Vector3i newMin;
+		GC_Vector3i newMax;
+		bool valid = true;
+
+		for_index(auto r : rangeStrings)
+		{
+			auto mm = GC_StrSplit<2>(r.Range(2), "..");
+			newMin[i] = GC_Atoi(mm[0]);
+			newMax[i] = GC_Atoi(mm[1]);
+
+			if (newMax[i] < newMin[i])
+				GC_Swap(newMin[i], newMax[i]);
+
+			if (newMin[i] < -aRange || newMax[i] > aRange)
+			{
+				valid = false;
+				break;
+			}
+		}
+
+		if (valid)
+		{
+			cubes.Flip(newMin, newMax);
+
+			if (parts[0][1] == 'f')
+				cubes.FlipRegion.PopBack();
+		}
 	}
 
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
-
-	}
-
-	return result;
+	return locVolume(cubes.min, cubes.max) - cubes.Volume();
 }
 
 DEFINE_TEST_G(Part1, Day22)
 {
-	TEST_EQ(locPart1("AOC_Day22Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day22Part1.txt"), 0);
-}
-
-static auto locPart2(char const*)
-{
-	uint64 result = 0;
-	return result;
+	TEST_EQ(locPart1("AOC_Day22Test.txt", 50), 590784);
+	TEST_EQ(locPart1("AOC_Day22Part1.txt", 50), 600458);
 }
 
 DEFINE_TEST_G(Part2, Day22)
 {
-	TEST_EQ(locPart2("AOC_Day22Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day22Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day22Test2.txt", 1000000), 2758514936282235);
+	TEST_EQ(locPart1("AOC_Day22Part1.txt", 1000000), 1334275219162622);
 }
