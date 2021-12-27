0ba97d3c475b9755efee635eae95217337f6505a
Chris Savoie <chris@chrissavoie.com>
2021-12-26 20:16:34 -0800

    Day 24 optimized, all tests running in under 1s

--- AOC/AOC_Day24.cpp
+++ AOC/AOC_Day24.cpp
@@ -44,6 +44,7 @@ struct processor
 	processor(char const* aFile)
 	{
 		instructions = locParseData(aFile);
+		Blocks = instructions.Count() / 14;
 	}
 
 	GC_DynamicArray<instruction> instructions;
@@ -74,58 +75,75 @@ struct processor
 			break;
 		}
 	}
+
+	const uint Div = 4;
+	const uint ModAdd = 5;
+	const uint ValAdd = 13;
+	uint Blocks;
+
+	int GetDiv(uint block) const { return instructions[block*Blocks + Div].value2; }
+	int GetModAdd(uint block) const { return instructions[block*Blocks + ModAdd].value2; }
+	int GetValAdd(uint block) const { return instructions[block*Blocks + ValAdd].value2; }
+
+	template <typename T>
+	T Run(uint block, T z, int val) const
+	{
+		GC_Vector4<T> reg(0, 0, z, val);
+		for (int i = block*Blocks + 1, e = block*Blocks + 18; i < e; ++i)
+			Step(reg, i);
+		return reg.z;
+	}
 };
 
-static uint64 locGetMaxValue(processor const& p, uint64 z, uint ins, GC_HashMap<GC_Pair<uint64,uint>, uint64>& memo, char const* aRange)
-{
-	if (uint64 const* val = memo.Find({ z, ins }))
-		return *val;
-
-	for (char const* c = aRange; *c; ++c)
-	{
-		GC_Vector4<s64> reg(0, 0, z, *c - '0');
-		for (uint i = ins + 1; i < ins + 18; ++i)
-			p.Step(reg, i);
-
-		if (ins == p.instructions.Count() - 18)
-		{
-			if (reg.z == 0)
-			{
-				GC_Pair<uint64, uint> key(z, ins);
-				memo.Add(key, reg.w);
-				return reg.w;
-			}
-		}
-		else
-		{
-			if (uint64 sub = locGetMaxValue(p, (uint)reg.z, ins + 18, memo, aRange))
-			{
-				uint64 n = sub * 10 + reg.w;
-				memo.Add({ z, ins }, n);
-				return n;
-			}
-		}
-	}
-
-	memo.Add({ z, ins }, 0);
-	return 0;
-}
-
-static auto locPart1(char const* aFile, char const* aRange)
+static auto locPart1(char const* aFile, char const* someDigits)
 {
 	processor p(aFile);
 
-	GC_HashMap<GC_Pair<uint64, uint>, uint64> memo;
-	memo.Reserve(50000000);
-	auto v = locGetMaxValue(p, 0, 0, memo, aRange);
+	GC_HybridArray<GC_Pair<int, int>, 7> pairs;
+	GC_HybridArray<int, 7> pairStack;
+	GC_HybridArray<int, 9> orderedDigits;
+	for_range(9) orderedDigits.Add(someDigits[i] - '0');
 
-	uint64 n = 0;
 	for_range(14)
 	{
-		n = n * 10 + v % 10;
-		v /= 10;
+		if (p.GetDiv(i) == 1)
+			pairStack.PushBack(i);
+		else
+		{
+			pairs.Add({ pairStack.Last(), i });
+			pairStack.PopBack();
+		}
 	}
-	return n;
+
+	GC_ASSERT(pairStack.Count() == 0 && pairs.Count() == 7);
+
+	uint64 digits[14];
+
+	for (auto pair : pairs)
+	{
+		for_range(9)
+		{
+			int z = p.Run(pair.myFirst, 0, orderedDigits[i]);
+			bool found = false;
+
+			for_range_v(j, 9)
+			{
+				if (p.Run(pair.mySecond, z, orderedDigits[j]) == 0)
+				{
+					found = true;
+					digits[pair.myFirst] = orderedDigits[i];
+					digits[pair.mySecond] = orderedDigits[j];
+				}
+			}
+
+			if (found) break;
+		}
+	}
+
+	uint64 result = 0;
+	for (uint64 d : digits)
+		result = result * 10 + d;
+	return result;
 }
 
 DEFINE_TEST_G(Part1, Day24)
