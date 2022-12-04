2121ae2af670f867d5175369a30e893fbab0a0c0
Chris Savoie <chris@chrissavoie.com>
2022-12-03 21:07:01 -0800

    Day 4

--- AOC/AOC_Day4.cpp
+++ AOC/AOC_Day4.cpp
@@ -1,34 +1,31 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
-{
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
-}
-
 static auto locPart1(char const* aFile)
 {
 	uint64 result = 0;
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
-
 	// By line parsing
 	for (auto line : GC_File::ReadAllLines(aFile))
 	{
-	}
+		auto parts = GC_StrSplit<2>(line, ',');
 
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
+		int min1 = 0, max1 = 0;
+		{
+			auto minmax = GC_StrSplit<2>(parts[0], '-');
+			min1 = GC_Atoi(minmax[0]);
+			max1 = GC_Atoi(minmax[1]);
+		}
 
+		int min2 = 0, max2 = 0;
+		{
+			auto minmax = GC_StrSplit<2>(parts[1], '-');
+			min2 = GC_Atoi(minmax[0]);
+			max2 = GC_Atoi(minmax[1]);
+		}
+
+		if ((min1 >= min2 && max1 <= max2) ||
+			(min2 >= min1 && max2 <= max1))
+			++result;
 	}
 
 	return result;
@@ -36,19 +33,42 @@ static auto locPart1(char const* aFile)
 
 DEFINE_TEST_G(Part1, Day4)
 {
-	TEST_EQ(locPart1("AOC_Day4Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day4Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day4Test.txt"), 2);
+	TEST_EQ(locPart1("AOC_Day4Part1.txt"), 518);
 }
 
 static auto locPart2(char const* aFile)
 {
-	(void)aFile;
 	uint64 result = 0;
+
+	// By line parsing
+	for (auto line : GC_File::ReadAllLines(aFile))
+	{
+		auto parts = GC_StrSplit<2>(line, ',');
+
+		int min1 = 0, max1 = 0;
+		{
+			auto minmax = GC_StrSplit<2>(parts[0], '-');
+			min1 = GC_Atoi(minmax[0]);
+			max1 = GC_Atoi(minmax[1]);
+		}
+
+		int min2 = 0, max2 = 0;
+		{
+			auto minmax = GC_StrSplit<2>(parts[1], '-');
+			min2 = GC_Atoi(minmax[0]);
+			max2 = GC_Atoi(minmax[1]);
+		}
+
+		if (!(max1 < min2 || min1 > max2))
+			++result;
+	}
+
 	return result;
 }
 
 DEFINE_TEST_G(Part2, Day4)
 {
-	TEST_EQ(locPart2("AOC_Day4Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day4Part1.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day4Test.txt"), 4);
+	TEST_EQ(locPart2("AOC_Day4Part1.txt"), 909);
 }
