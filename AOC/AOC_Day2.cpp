05911942ea7a6b7a67a447fbe102e4cd9e001df5
Chris Savoie <chris@chrissavoie.com>
2022-12-07 21:56:51 -0800

    Regenerate data for 2015

--- AOC/AOC_Day2.cpp
+++ AOC/AOC_Day2.cpp
@@ -1,26 +1,34 @@
 #include "AOC_Precompiled.h"
 
+static auto locParseData(char const* aFile)
+{
+	// By line with parse function
+	return GC_File::Parse<int>(aFile, [](auto aLine)
+		{
+			return GC_Atoi(aLine);
+		});
+}
+
 static auto locPart1(char const* aFile)
 {
 	uint64 result = 0;
 
-	const char win[3] = { 1, 2, 0 };
+	for (auto item : locParseData(aFile))
+	{
+		(void)item;
+	}
 
 	// By line parsing
-	auto lines = GC_File::ReadAllLines(aFile);
-	for (auto line : lines)
+	for (auto line : GC_File::ReadAllLines(aFile))
 	{
-		GC_StrSlice part;
-		GC_Strtok(line, ' ', part);
-		char a = part[0] - 'A';
-		GC_Strtok(line, ' ', part);
-		char c = part[0] - 'X';
+	}
 
-		result += c + 1;
-		if (a == c)
-			result += 3;
-		if (win[a] == c)
-			result += 6;
+	// By Block parsing (block of lines separate by two new lines)
+	GC_String text;
+	GC_File::ReadAllText(aFile, text);
+	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
+	{
+
 	}
 
 	return result;
@@ -28,52 +36,19 @@ static auto locPart1(char const* aFile)
 
 DEFINE_TEST_G(Part1, Day2)
 {
-	TEST_EQ(locPart1("AOC_Day2Test.txt"), 15);
-	TEST_EQ(locPart1("AOC_Day2Part1.txt"), 10595);
+	TEST_EQ(locPart1("AOC_Day2Test.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day2Part1.txt"), 0);
 }
 
 static auto locPart2(char const* aFile)
 {
+	(void)aFile;
 	uint64 result = 0;
-
-	const char win[3] = { 1, 2, 0 };
-
-	// By line parsing
-	auto lines = GC_File::ReadAllLines(aFile);
-	for (auto line : lines)
-	{
-		GC_StrSlice part;
-		GC_Strtok(line, ' ', part);
-		char a = part[0] - 'A';
-		GC_Strtok(line, ' ', part);
-		char w = part[0] - 'X';
-		char c = 0;
-
-		switch (w)
-		{
-		case 0:
-			c = (a + 2) % 3;
-			break;
-		case 1:
-			c = a;
-			break;
-		case 2:
-			c = win[a];
-			break;
-		}
-
-		result += c + 1;
-		if (a == c)
-			result += 3;
-		if (win[a] == c)
-			result += 6;
-	}
-
 	return result;
 }
 
 DEFINE_TEST_G(Part2, Day2)
 {
-	TEST_EQ(locPart2("AOC_Day2Test.txt"), 12);
+	TEST_EQ(locPart2("AOC_Day2Test.txt"), 0);
 	TEST_EQ(locPart2("AOC_Day2Part1.txt"), 0);
 }
--- AOC/AOC_Day8.cpp
+++ AOC/AOC_Day8.cpp
@@ -1,143 +1,54 @@
 #include "AOC_Precompiled.h"
 
+static auto locParseData(char const* aFile)
+{
+	// By line with parse function
+	return GC_File::Parse<int>(aFile, [](auto aLine)
+		{
+			return GC_Atoi(aLine);
+		});
+}
+
 static auto locPart1(char const* aFile)
 {
-	auto data = GC_File::Parse2d<char>(aFile, [](char c) -> char { return c - '0'; });
-	GC_DynamicArray2D<bool> visited;
-	visited.SetSize(data.Size());
-	GC_MemZero(visited.begin(), visited.SizeInBytes());
+	uint64 result = 0;
 
-	uint64 result = (data.Width() + data.Height() - 2) * 2;
-
-	for (uint y = 1, e = data.Height() - 1; y < e; ++y)
+	for (auto item : locParseData(aFile))
 	{
-		auto row = data.Row(y);
-		char min = row.First();
-		char max = row.Last();
-		uint index = 1;
-		for (; index < row.Count() - 1 && min < 9; ++index)
-		{
-			if (char c = row[index]; c > min)
-			{
-				if (!visited(index, y))
-				{
-					visited(index, y) = true;
-					++result;
-				}
-				min = c;
-			}
-		}
-		uint end = row.Count() - 2;
-		for (; end > 0 && max < 9; --end)
-		{
-			if (char c = row[end]; c > max)
-			{
-				if (!visited(end, y))
-				{
-					visited(end, y) = true;
-					++result;
-				}
-				max = c;
-			}
-		}
+		(void)item;
 	}
 
-	for (uint y = 1, e = data.Width() - 1; y < e; ++y)
+	// By line parsing
+	for (auto line : GC_File::ReadAllLines(aFile))
 	{
-		auto row = data.Column(y);
-		char min = *row.begin();
-		char max = *(row.end()-1);
-		uint index = 1;
-		for (; index < row.Count() - 1 && min < 9; ++index)
-		{
-			if (char c = row[index]; c > min)
-			{
-				if (!visited(y, index))
-				{
-					visited(y, index) = true;
-					++result;
-				}
-				min = c;
-			}
-		}
-		uint end = row.Count() - 2;
-		for (; end > 0 && max < 9; --end)
-		{
-			if (char c = row[end]; c > max)
-			{
-				if (!visited(y, end))
-				{
-					visited(y, end) = true;
-					++result;
-				}
-				max = c;
-			}
-		}
 	}
 
-	//for_range_v(y, data.Height())
-	//{
-	//	for_range_v(x, data.Width())
-	//	{
-	//		if (visited(x, y))
-	//		{
-	//			printf(ANSI_COLOR_YELLOW "%d", data(x, y));
-	//		}
-	//		else
-	//		{
-	//			printf(ANSI_COLOR_RESET "%d", data(x, y));
-	//		}
-	//	}
+	// By Block parsing (block of lines separate by two new lines)
+	GC_String text;
+	GC_File::ReadAllText(aFile, text);
+	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
+	{
 
-	//	printf("\n");
-	//}
+	}
 
 	return result;
 }
 
 DEFINE_TEST_G(Part1, Day8)
 {
-	TEST_EQ(locPart1("AOC_Day8Test.txt"), 21);
-	TEST_EQ(locPart1("AOC_Day8Part1.txt"), 1779);
+	TEST_EQ(locPart1("AOC_Day8Test.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day8Part1.txt"), 0);
 }
 
 static auto locPart2(char const* aFile)
 {
-	auto data = GC_File::Parse2d<char>(aFile, [](char c) -> char { return c - '0'; });
-
+	(void)aFile;
 	uint64 result = 0;
-
-	int const w = data.Width(), h = data.Height();
-
-	for_range2d(data.Width(), data.Height())
-	{
-		char c = data(x, y);
-
-		uint64 dist[GC_Cardinal::Count] = { 0 };
-
-		for (GC_Cardinal dire : GC_Cardinal::Range())
-		{
-			GC_Vector2i dir = GC_CardinalDirection(dire);
-			GC_Vector2i p = GC_Vector2i(x, y) + dir;
-			while (p.x >= 0 && p.x < w && p.y >= 0 && p.y < h)
-			{
-				dist[dire]++;
-
-				if (data(p) >= c)
-					break;
-
-				p += dir;
-			}
-		}
-
-		result = GC_Max(result, GC_Algorithm::Product(dist));
-	}
-
 	return result;
 }
 
 DEFINE_TEST_G(Part2, Day8)
 {
-	TEST_EQ(locPart2("AOC_Day8Test.txt"), 8);
-	TEST_EQ(locPart2("AOC_Day8Part1.txt"), 172224);
+	TEST_EQ(locPart2("AOC_Day8Test.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day8Part1.txt"), 0);
 }
