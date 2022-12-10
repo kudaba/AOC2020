5f9828cb06ac1272cb984cf43e687d7e874e1e8f
Chris Savoie <chris@chrissavoie.com>
2022-12-09 22:18:22 -0800

    Day 10

--- AOC/AOC_Day10.cpp
+++ AOC/AOC_Day10.cpp
@@ -1,54 +1,57 @@
 #include "AOC_Precompiled.h"
-
-static auto locParseData(char const* aFile)
-{
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
-}
+#include "GC_StringBuilder.h"
 
 static auto locPart1(char const* aFile)
 {
-	uint64 result = 0;
+	int64 result = 0;
+	int64 cycle = 0;
+	int64 X = 1;
+	int64 next = 20;
+	int64 nextLine = 40;
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
+	GC_StringBuilderLarge output;
 
-	// By line parsing
 	for (auto line : GC_File::ReadAllLines(aFile))
 	{
+		int64 nextCycle = cycle + 1;
+		int64 nextX = X;
+
+		if (line[0] != 'n')
+		{
+			++nextCycle;
+			nextX += GC_Atoi(line.Range(5));
+		}
+
+		while (cycle < nextCycle)
+		{
+			output.Append((GC_Abs((cycle % 40) - X) <= 1) ? "#" : ".");
+
+			++cycle;
+
+			if (cycle == next)
+			{
+				result += X * next;
+				next += 40;
+			}
+
+			if (cycle == nextLine)
+			{
+				output.Append("\n");
+				nextLine += 40;
+			}
+		}
+
+		X = nextX;
 	}
 
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
-
-	}
+	output.Append("\n");
+	printf(output.ToString());
 
 	return result;
 }
 
 DEFINE_TEST_G(Part1, Day10)
 {
-	TEST_EQ(locPart1("AOC_Day10Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day10Part1.txt"), 0);
-}
-
-static auto locPart2(char const* aFile)
-{
-	(void)aFile;
-	uint64 result = 0;
-	return result;
-}
-
-DEFINE_TEST_G(Part2, Day10)
-{
-	TEST_EQ(locPart2("AOC_Day10Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day10Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day10Test.txt"), 13140);
+	TEST_EQ(locPart1("AOC_Day10Part1.txt"), 15680);
 }
