2f401a911e830cb24382af49dfea39c509aafa3b
Chris Savoie <chris@chrissavoie.com>
2022-12-22 21:37:51 -0800

    Day 23 (needs optimization)

--- AOC/AOC_Day23.cpp
+++ AOC/AOC_Day23.cpp
@@ -1,54 +1,194 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
+static auto locPart1(char const* aFile, uint rounds)
 {
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
+	auto map = GC_File::Parse2d<bool>(aFile, [](char c) { return c == '#'; });
+
+	struct Elf
+	{
+		GC_Optional<GC_Cardinal> Proposed;
+	};
+
+	GC_HashMap<GC_Vector2i, Elf> elves;
+	for_range2d(map.Width(), map.Height())
+		if (map(x, y))
+			elves.Add(GC_Vector2i(x, y), {});
+
+	GC_Function<bool(GC_Vector2i)> rules[] =
+	{
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(-1,1)) || elves.Contains(p + GC_Vector2(0,1)) || elves.Contains(p + GC_Vector2(1,1))); },
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(1,-1)) || elves.Contains(p + GC_Vector2(1,0)) || elves.Contains(p + GC_Vector2(1,1))); },
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(-1,-1)) || elves.Contains(p + GC_Vector2(0,-1)) || elves.Contains(p + GC_Vector2(1,-1))); },
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(-1,-1)) || elves.Contains(p + GC_Vector2(-1,0)) || elves.Contains(p + GC_Vector2(-1,1))); },
+	};
+	GC_Cardinal Dirs[4] = { GC_Cardinal::South, GC_Cardinal::North, GC_Cardinal::West, GC_Cardinal::East };
+
+	GC_HashMap<GC_Vector2i, uint> proposals;
+	GC_HashMap<GC_Vector2i, Elf> elves2;
+	elves2.Reserve(elves.Count());
+	for_range_v(r, rounds)
+	{
+		proposals.Clear();
+
+		for (auto& elf : elves)
 		{
-			return GC_Atoi(aLine);
-		});
-}
+			bool any = false;
+			for_range2d(3, 3)
+			{
+				GC_Vector2i off(x - 1, y - 1);
+				if ((off.x || off.y) && elves.Contains(elf.Key + off))
+				{
+					any = true;
+					break;
+				}
+			}
 
-static auto locPart1(char const* aFile)
-{
-	uint64 result = 0;
+			if (any)
+			for_range(4)
+			{
+				GC_Cardinal dir = Dirs[(i + r) % 4];
+				if (rules[dir](elf.Key))
+				{
+					elf.Value.Proposed = dir;
+					GC_Vector2u n = elf.Key + GC_CardinalDirection(elf.Value.Proposed.Get());
+					if (uint* p = proposals.Find(n))
+						(*p)++;
+					else
+						proposals.Add(n, 0);
+					break;
+				}
+			}
+		}
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
+		elves2.Clear();
+
+		for (auto& elf : elves)
+		{
+			GC_Vector2i np = elf.Key;
+
+			if (elf.Value.Proposed.IsSet())
+			{
+				GC_Vector2u n = elf.Key + GC_CardinalDirection(elf.Value.Proposed.Get());
+				if (proposals[n] == 0)
+				{
+					np = n;
+				}
+			}
+
+			elves2.Add(np, Elf());
+		}
+
+		elves.Swap(elves2);
 	}
 
-	// By line parsing
-	for (auto line : GC_File::ReadAllLines(aFile))
+	GC_Vector2i min(INT_MAX), max(INT_MIN);
+	for (auto& elf : elves)
 	{
+		min = GC_Min(min, elf.Key);
+		max = GC_Max(max, elf.Key);
 	}
 
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
-
-	}
-
-	return result;
+	return (max - min + GC_Vector2i(1)).Area() - elves.Count();
 }
 
 DEFINE_TEST_G(Part1, Day23)
 {
-	TEST_EQ(locPart1("AOC_Day23Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day23Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day23Test.txt", 10), 110);
+	TEST_EQ(locPart1("AOC_Day23Part1.txt", 10), 3815);
 }
 
 static auto locPart2(char const* aFile)
 {
-	(void)aFile;
-	uint64 result = 0;
-	return result;
+	auto map = GC_File::Parse2d<bool>(aFile, [](char c) { return c == '#'; });
+
+	struct Elf
+	{
+		GC_Optional<GC_Cardinal> Proposed;
+	};
+
+	GC_HashMap<GC_Vector2i, Elf> elves;
+	for_range2d(map.Width(), map.Height())
+		if (map(x, y))
+			elves.Add(GC_Vector2i(x, y), {});
+
+	GC_Function<bool(GC_Vector2i)> rules[] =
+	{
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(-1,1)) || elves.Contains(p + GC_Vector2(0,1)) || elves.Contains(p + GC_Vector2(1,1))); },
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(1,-1)) || elves.Contains(p + GC_Vector2(1,0)) || elves.Contains(p + GC_Vector2(1,1))); },
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(-1,-1)) || elves.Contains(p + GC_Vector2(0,-1)) || elves.Contains(p + GC_Vector2(1,-1))); },
+		[&](GC_Vector2i p) { return !(elves.Contains(p + GC_Vector2(-1,-1)) || elves.Contains(p + GC_Vector2(-1,0)) || elves.Contains(p + GC_Vector2(-1,1))); },
+	};
+	GC_Cardinal Dirs[4] = { GC_Cardinal::South, GC_Cardinal::North, GC_Cardinal::West, GC_Cardinal::East };
+
+	GC_HashMap<GC_Vector2i, uint> proposals;
+	GC_HashMap<GC_Vector2i, Elf> elves2;
+	elves2.Reserve(elves.Count());
+	uint round = 0;
+	while(1)
+	{
+		proposals.Clear();
+
+		for (auto& elf : elves)
+		{
+			bool any = false;
+			for_range2d(3, 3)
+			{
+				GC_Vector2i off(x - 1, y - 1);
+				if ((off.x || off.y) && elves.Contains(elf.Key + off))
+				{
+					any = true;
+					break;
+				}
+			}
+
+			if (any)
+				for_range(4)
+			{
+				GC_Cardinal dir = Dirs[(i + round) % 4];
+				if (rules[dir](elf.Key))
+				{
+					elf.Value.Proposed = dir;
+					GC_Vector2u n = elf.Key + GC_CardinalDirection(elf.Value.Proposed.Get());
+					if (uint* p = proposals.Find(n))
+						(*p)++;
+					else
+						proposals.Add(n, 0);
+					break;
+				}
+			}
+		}
+
+		++round;
+
+		if (proposals.Count() == 0)
+			return round;
+
+		elves2.Clear();
+
+		for (auto& elf : elves)
+		{
+			GC_Vector2i np = elf.Key;
+
+			if (elf.Value.Proposed.IsSet())
+			{
+				GC_Vector2u n = elf.Key + GC_CardinalDirection(elf.Value.Proposed.Get());
+				if (proposals[n] == 0)
+				{
+					np = n;
+				}
+			}
+
+			elves2.Add(np, Elf());
+		}
+
+		elves.Swap(elves2);
+	}
+
+	return 0u;
 }
 
 DEFINE_TEST_G(Part2, Day23)
 {
-	TEST_EQ(locPart2("AOC_Day23Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day23Part1.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day23Test.txt"), 20);
+	TEST_EQ(locPart2("AOC_Day23Part1.txt"), 893);
 }
