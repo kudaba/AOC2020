ab98a88e9e32ce069b0d1a4e23abd2deba00aff2
EPICGAMES\christian.savoie <chris@chrissavoie.com>
2023-12-09 01:12:35 -0400

    Day 1

--- AOC/AOC_Day1.cpp
+++ AOC/AOC_Day1.cpp
@@ -1,34 +1,31 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
-{
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
-}
-
 static auto locPart1(char const* aFile)
 {
 	uint64 result = 0;
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
-
 	// By line parsing
 	for (auto line : GC_File::ReadAllLines(aFile))
 	{
-	}
+		int first = -1;
+		int last = -1;
+		for (char c : line)
+		{
+			if (GC_IsDigit(c))
+			{
+				if (first == -1)
+				{
+					first = c - '0';
+					last = first;
+				}
+				else
+				{
+					last = c - '0';
+				}
+			}
+		}
 
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
-
+		result += first * 10 + last;
 	}
 
 	return result;
@@ -36,19 +33,74 @@ static auto locPart1(char const* aFile)
 
 DEFINE_TEST_G(Part1, Day1)
 {
-	TEST_EQ(locPart1("AOC_Day1Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day1Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day1Test.txt"), 142);
+	TEST_EQ(locPart1("AOC_Day1Part1.txt"), 54573);
 }
 
 static auto locPart2(char const* aFile)
 {
-	(void)aFile;
 	uint64 result = 0;
+
+	GC_StaticString<32> Numbers[] =
+	{
+		"-----",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	};
+
+	// By line parsing
+	for (GC_String const& line : GC_File::ReadAllLines(aFile))
+	{
+		int first = -1;
+		int last = -1;
+		for_range(line.Length())
+		{
+			int value = -1;
+			if (GC_IsDigit(line[i]))
+			{
+				value = line[i] - '0';
+			}
+			else
+			{
+				for_index_v(j, auto const& num : Numbers)
+				{
+					if (GC_Strnieq(num, line.ToString() + i, num.Length()))
+					{
+						value = j;
+						break;
+					}
+				}
+			}
+
+			if (value != -1)
+			{
+				if (first == -1)
+				{
+					first = value;
+					last = first;
+				}
+				else
+ 				{
+					last = value;
+				}
+			}
+		}
+
+		result += first * 10 + last;
+	}
+
 	return result;
 }
 
 DEFINE_TEST_G(Part2, Day1)
 {
-	TEST_EQ(locPart2("AOC_Day1Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day1Part1.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day1Test2.txt"), 281);
+	TEST_EQ(locPart2("AOC_Day1Part1.txt"), 54591);
 }
