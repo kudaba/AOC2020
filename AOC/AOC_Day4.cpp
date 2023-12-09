eb8e4f8b99bf4eea1e43614bf67ceae4ad2f1388
EPICGAMES\christian.savoie <chris@chrissavoie.com>
2023-12-09 14:58:01 -0400

    Day 4

--- AOC/AOC_Day4.cpp
+++ AOC/AOC_Day4.cpp
@@ -1,34 +1,31 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
-{
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
-}
-
 static auto locPart1(char const* aFile)
 {
 	uint64 result = 0;
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
+	GC_DynamicArray<uint> winningNumbers;
 
 	// By line parsing
 	for (auto line : GC_File::ReadAllLines(aFile))
 	{
-	}
+		winningNumbers.Clear();
 
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
+		auto parts = GC_StrSplit<2>(line, " | ");
 
+		for (auto number : GC_StrSplit<32>(GC_StrSplit<2>(parts[0], ": ")[1], ' '))
+			winningNumbers.Add(GC_Atoi(number));
+
+		uint score = 0;
+		for (auto number : GC_StrSplit<32>(parts[1], ' '))
+		{
+			if (winningNumbers.Contains(GC_Atou(number)))
+			{
+				score = score * 2 + !score;
+			}
+		}
+
+		result += score;
 	}
 
 	return result;
@@ -36,19 +33,57 @@ static auto locPart1(char const* aFile)
 
 DEFINE_TEST_G(Part1, Day4)
 {
-	TEST_EQ(locPart1("AOC_Day4Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day4Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day4Test.txt"), 13);
+	TEST_EQ(locPart1("AOC_Day4Part1.txt"), 27845);
 }
 
 static auto locPart2(char const* aFile)
 {
-	(void)aFile;
 	uint64 result = 0;
+
+	GC_DynamicArray<uint> winningNumbers;
+	GC_DynamicArray<uint> cards;
+
+	auto tokens = GC_File::ReadAllLines(aFile);
+	auto lines = tokens.GetAll();
+	cards.Resize(lines.Count(), 1);
+
+
+	// By line parsing
+	for_index(auto line : lines)
+	{
+		result += cards[i];
+
+		winningNumbers.Clear();
+
+		auto parts = GC_StrSplit<2>(line, " | ");
+
+		for (auto number : GC_StrSplit<32>(GC_StrSplit<2>(parts[0], ": ")[1], ' '))
+			winningNumbers.Add(GC_Atoi(number));
+
+		uint score = 0;
+		for (auto number : GC_StrSplit<32>(parts[1], ' '))
+		{
+			if (winningNumbers.Contains(GC_Atou(number)))
+			{
+				++score;
+			}
+		}
+
+		for_range_v(j, score)
+		{
+			if (i + j + 1 < cards.Count())
+			{
+				cards[i + j + 1] += cards[i];
+			}
+		}
+	}
+
 	return result;
 }
 
 DEFINE_TEST_G(Part2, Day4)
 {
-	TEST_EQ(locPart2("AOC_Day4Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day4Part1.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day4Test.txt"), 30);
+	TEST_EQ(locPart2("AOC_Day4Part1.txt"), 9496801);
 }
