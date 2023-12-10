6957222dc2bf1d57af101c0af9a527bc2146be7d
EPICGAMES\christian.savoie <chris@chrissavoie.com>
2023-12-09 20:20:17 -0400

    Day 6 (optimized)

--- AOC/AOC_Day6.cpp
+++ AOC/AOC_Day6.cpp
@@ -1,34 +1,32 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
-{
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
-}
-
 static auto locPart1(char const* aFile)
 {
-	uint64 result = 0;
-
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
-
-	// By line parsing
-	for (auto line : GC_File::ReadAllLines(aFile))
-	{
-	}
+	uint64 result = 1;
 
 	// By Block parsing (block of lines separate by two new lines)
 	GC_String text;
 	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
+	auto parts = GC_StrSplit<2>(text, '\n');
+	auto timeParts = GC_StrSplit<4>(parts[0], ' ', false);
+	auto distParts = GC_StrSplit<4>(parts[1], ' ', false);
 
+	for_range(timeParts.Count())
+	{
+		if (i)
+		{
+			uint time = GC_Atoi(timeParts[i]);
+			uint dist = GC_Atoi(distParts[i]);
+
+			uint ways = 0;
+			for_range_v(j, time)
+			{
+				if (j * (time - j) > dist)
+					++ways;
+			}
+
+			result *= ways;
+		}
 	}
 
 	return result;
@@ -36,19 +34,69 @@ static auto locPart1(char const* aFile)
 
 DEFINE_TEST_G(Part1, Day6)
 {
-	TEST_EQ(locPart1("AOC_Day6Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day6Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day6Test.txt"), 288);
+	TEST_EQ(locPart1("AOC_Day6Part1.txt"), 303600);
 }
 
 static auto locPart2(char const* aFile)
 {
-	(void)aFile;
-	uint64 result = 0;
-	return result;
+	// By Block parsing (block of lines separate by two new lines)
+	GC_String text;
+	GC_File::ReadAllText(aFile, text);
+	auto parts = GC_StrSplit<2>(text, '\n');
+	auto timeParts = GC_StrSplit<4>(parts[0], ' ', false);
+	auto distParts = GC_StrSplit<4>(parts[1], ' ', false);
+
+	uint64 time = 0;
+	uint64 dist = 0;
+
+	for_range(timeParts.Count())
+	{
+		if (i)
+		{
+			for (char c : timeParts[i])
+				time = time * 10 + (c - '0');
+
+			for (char c : distParts[i])
+				dist = dist * 10 + (c - '0');
+		}
+	}
+
+	uint64 lower = 0;
+	{
+		uint64 min = 0, max = time, cur = time / 2;
+		while (min < max - 1)
+		{
+			if (cur * (time - cur) > dist)
+				max = cur;
+			else
+				min = cur;
+
+			cur = min + (max - min) / 2;
+		}
+		lower = min;
+	}
+
+	uint64 upper = 0;
+	{
+		uint64 min = 0, max = time, cur = time / 2;
+		while (min < max - 1)
+		{
+			if (cur * (time - cur) > dist)
+				min = cur;
+			else
+				max = cur;
+
+			cur = min + (max - min) / 2;
+		}
+		upper = min;
+	}
+
+	return upper - lower;
 }
 
 DEFINE_TEST_G(Part2, Day6)
 {
-	TEST_EQ(locPart2("AOC_Day6Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day6Part1.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day6Test.txt"), 71503);
+	TEST_EQ(locPart2("AOC_Day6Part1.txt"), 23654842);
 }
