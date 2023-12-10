4703d3422712994d609bcad4987bb01761ef1be0
EPICGAMES\christian.savoie <chris@chrissavoie.com>
2023-12-10 10:12:39 -0400

    Day 9

--- AOC/AOC_Day9.cpp
+++ AOC/AOC_Day9.cpp
@@ -1,34 +1,42 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
+static int64 NextItemInSequence(GC_DynamicArray<int64> const& aSequence)
 {
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
+	GC_DynamicArray<int64> nextSequence;
+	nextSequence.Resize(aSequence.Count() - 1);
+
+	bool allZeros = true;
+
+	for_range(nextSequence.Count())
+	{
+		const int64 next = aSequence[i + 1] - aSequence[i];
+		allZeros &= !next;
+		nextSequence[i] = next;
+	}
+
+	if (allZeros)
+	{
+		return aSequence.Last();
+	}
+
+	return aSequence.Last() + NextItemInSequence(nextSequence);
 }
 
 static auto locPart1(char const* aFile)
 {
-	uint64 result = 0;
+	int64 result = 0;
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
-
-	// By line parsing
+	GC_DynamicArray<int64> sequence;
 	for (auto line : GC_File::ReadAllLines(aFile))
 	{
-	}
+		sequence.Clear();
 
-	// By Block parsing (block of lines separate by two new lines)
-	GC_String text;
-	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
-	{
+		for (auto n : GC_StrSplit<32>(line, ' '))
+		{
+			sequence.Add(GC_Atoi(n));
+		}
 
+		result += NextItemInSequence(sequence);
 	}
 
 	return result;
@@ -36,19 +44,32 @@ static auto locPart1(char const* aFile)
 
 DEFINE_TEST_G(Part1, Day9)
 {
-	TEST_EQ(locPart1("AOC_Day9Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day9Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day9Test.txt"), 114);
+	TEST_EQ(locPart1("AOC_Day9Part1.txt"), 1884768153);
 }
 
 static auto locPart2(char const* aFile)
 {
-	(void)aFile;
-	uint64 result = 0;
+	int64 result = 0;
+
+	GC_DynamicArray<int64> sequence;
+	for (auto line : GC_File::ReadAllLines(aFile))
+	{
+		sequence.Clear();
+
+		for (auto n : GC_StrSplit<32>(line, ' '))
+		{
+			sequence.Insert(0, GC_Atoi(n));
+		}
+
+		result += NextItemInSequence(sequence);
+	}
+
 	return result;
 }
 
 DEFINE_TEST_G(Part2, Day9)
 {
-	TEST_EQ(locPart2("AOC_Day9Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day9Part1.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day9Test.txt"), 2);
+	TEST_EQ(locPart2("AOC_Day9Part1.txt"), 1031);
 }
