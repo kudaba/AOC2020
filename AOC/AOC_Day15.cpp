88f2ebffa87eac357e198dac20c3622bd9067753
EPICGAMES\christian.savoie <chris@chrissavoie.com>
2023-12-15 08:58:44 -0400

    Day 15

--- AOC/AOC_Day15.cpp
+++ AOC/AOC_Day15.cpp
@@ -1,34 +1,27 @@
 #include "AOC_Precompiled.h"
 
-static auto locParseData(char const* aFile)
+static uint locHash(GC_StrSlice str)
 {
-	// By line with parse function
-	return GC_File::Parse<int>(aFile, [](auto aLine)
-		{
-			return GC_Atoi(aLine);
-		});
+	uint hash = 0;
+
+	for (char c : str)
+	{
+		hash = ((hash + c) * 17) % 256;
+	}
+
+	return hash;
 }
 
 static auto locPart1(char const* aFile)
 {
 	uint64 result = 0;
 
-	for (auto item : locParseData(aFile))
-	{
-		(void)item;
-	}
-
-	// By line parsing
-	for (auto line : GC_File::ReadAllLines(aFile))
-	{
-	}
-
 	// By Block parsing (block of lines separate by two new lines)
 	GC_String text;
 	GC_File::ReadAllText(aFile, text);
-	for (GC_StrSlice chunk; GC_Strtok(text, "\n\n", chunk);)
+	for (GC_StrSlice chunk; GC_Strtoks(text, ",\n", chunk);)
 	{
-
+		result += locHash(chunk);
 	}
 
 	return result;
@@ -36,19 +29,61 @@ static auto locPart1(char const* aFile)
 
 DEFINE_TEST_G(Part1, Day15)
 {
-	TEST_EQ(locPart1("AOC_Day15Test.txt"), 0);
-	TEST_EQ(locPart1("AOC_Day15Part1.txt"), 0);
+	TEST_EQ(locPart1("AOC_Day15Test.txt"), 1320);
+	TEST_EQ(locPart1("AOC_Day15Part1.txt"), 511343);
 }
 
 static auto locPart2(char const* aFile)
 {
-	(void)aFile;
+	GC_DynamicArray<GC_Pair<GC_String, uint>> Boxes[256];
+
+	// By Block parsing (block of lines separate by two new lines)
+	GC_String text;
+	GC_File::ReadAllText(aFile, text);
+	for (GC_StrSlice chunk; GC_Strtoks(text, ",\n", chunk);)
+	{
+		if (char const* sub = GC_Strchr(chunk.Buffer(), '-', 0, chunk.Count()))
+		{
+			GC_String left = chunk.Left(chunk.Count() - 1);
+			uint hash = locHash(left.ToSlice());
+			int32 index = Boxes[hash].Find(left, [](auto& i, auto& s) { return i.First == s; });
+			if (index >= 0)
+			{
+				Boxes[hash].RemoveAt(index);
+			}
+		}
+		else
+		{
+			auto parts = GC_StrSplit<2>(chunk, '=');
+			uint focal = GC_Atoi(parts[1]);
+			uint hash = locHash(parts[0]);
+			int32 index = Boxes[hash].Find(parts[0], [](auto& i, auto& s) { return i.First == s; });
+			if (index >= 0)
+			{
+				Boxes[hash][index].Second = focal;
+			}
+			else
+			{
+				Boxes[hash].Add({ parts[0], focal });
+			}
+		}
+	}
+
 	uint64 result = 0;
+
+	for_index(auto& box : Boxes)
+	{
+		for_index_v(p, auto& pair : box)
+		{
+			result += (i + 1) * (p + 1) * pair.Second;
+		}
+	}
+
 	return result;
 }
 
 DEFINE_TEST_G(Part2, Day15)
 {
-	TEST_EQ(locPart2("AOC_Day15Test.txt"), 0);
-	TEST_EQ(locPart2("AOC_Day15Part1.txt"), 0);
+	TEST_EQ(locPart2("AOC_Day15Test.txt"), 145);
+	TEST_EQ(locPart2("AOC_Day15Part1.txt"), 294474);
 }
